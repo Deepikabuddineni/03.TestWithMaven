6c8766882819384c3d28c68599b379c221b61482
Deepikabuddineni <deeparaob@yahoo.com>
2017-07-14 16:51:09 -0400

    Threads

---
+++ src/main/java/com/fanniemae/payroll/EmailProcess.java
@@ -0,0 +1,28 @@
+package com.fanniemae.payroll;
+
+public class EmailProcess implements Runnable{
+	public void sendEmail(){
+	
+		System.out.println("Email process started");
+			
+try {
+	Thread.sleep(10_000);	
+} catch (InterruptedException e) {
+	e.printStackTrace();
+	
+	System.out.println("Email process ended");
+	
+	
+}
+
+		
+    }
+
+	@Override
+	public void run() {
+		sendEmail();
+		
+	}
+	
+	
+}
